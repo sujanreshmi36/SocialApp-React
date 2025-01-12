b6518d874cca6ab64479ddeffedbe6dc64aa3c96
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2025-01-12 16:10:54 +0545

    Topbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Home from "./pages/home/home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Topbar from "../Topbar/Topbar";
+const Home = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+};
+
+export default Home;
