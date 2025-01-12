8c1d4d7c8c15baa9226da32c28d63969f82600c9
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2025-01-12 17:56:42 +0545

    side bar created

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Topbar from "../Topbar/Topbar";
+import Sidebar from "../sidebar/Sidebar";
+import Feed from "../Feed/Feed";
+import Rightbar from "../rightbar/Rightbar";
+import "./home.css";
 const Home = () => {
   return (
     <div>
       <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
     </div>
   );
 };
---
+++ src/pages/rightbar/Rightbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Rightbar.css";
+const Rightbar = () => {
+  return <div className="rightbar">Rightbar</div>;
+};
+
+export default Rightbar;
